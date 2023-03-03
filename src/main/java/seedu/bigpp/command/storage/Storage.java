b173c13e7205165e3945ed12cfe336083e36e128
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2023-03-04 01:53:18 +0800

    add skeleton for storage and userdata

---
+++ src/main/java/seedu/bigpp/command/storage/Storage.java
@@ -0,0 +1,6 @@
+package seedu.bigpp.command.storage;
+
+public class Storage {
+    public static final String FOLDER_PATH = "./userdata";
+    public static final String FILE_PATH = "./userdata/bigpp.txt";
+}
