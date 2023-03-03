ae3fe87f3320f1746b0a85b8fafcce67a25a6016
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2023-03-04 01:33:17 +0800

    add UI handler and UI state enum

---
+++ src/main/java/seedu/bigpp/ui/UI.java
@@ -0,0 +1,8 @@
+package seedu.bigpp.ui;
+
+public class UI {
+    private static UIState uiState = UIState.MAIN_MENU;
+
+    public UI() {
+    }
+}
---
+++ src/main/java/seedu/bigpp/ui/UIState.java
@@ -0,0 +1,9 @@
+package seedu.bigpp.ui;
+
+public enum UIState {
+    MAIN_MENU,
+    VIEWER,
+    BUILDER,
+    TUTORIAL,
+    COMPONENT,
+}
