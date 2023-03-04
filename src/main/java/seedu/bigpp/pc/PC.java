77cce34e2a3ffc7164db0cd389c0b08f5e6768b9
Ryujikjs <ryujikjs@gmail.com>
2023-03-04 20:01:59 +0800

    Add method in PcList class to output formatted list and update PC class to support new output format

--- src/main/java/seedu/bigpp/pc/PC.java
+++ src/main/java/seedu/bigpp/pc/PC.java
@@ -1,20 +1,47 @@
 package seedu.bigpp.pc;
 
+import seedu.bigpp.component.cpu.CPU;
+
 public class PC {
     private String name;
     // private CPU cpu;
+    private Boolean isPrebuilt;
 
-    public PC(String name) {
+    private CPU cpu = null;
+
+    public PC(String name, Boolean isPrebuilt) {
         this.name = name;
         // this.cpu = new CPU();
+        this.isPrebuilt = isPrebuilt;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setCPU(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+    public Float getCost() {
+        Float totalCost = 0.00F;
+        if (cpu == null) {
+            return totalCost;
+        }
+        totalCost += cpu.getPrice();
+        return totalCost;
+    }
+
+    public String buildType(Boolean isPrebuilt) {
+        if (isPrebuilt) {
+            return "Prebuilt-PC";
+        } else {
+            return "Custom-PC";
+        }
+    }
+
     @Override
     public String toString() {
-        return "PC [name=" + name + "]";
+        return buildType(isPrebuilt) + " [" + name + "]" + " - $" + getCost();
     }
 }
--- src/main/java/seedu/bigpp/pc/PCList.java
+++ src/main/java/seedu/bigpp/pc/PCList.java
@@ -3,9 +3,20 @@ package seedu.bigpp.pc;
 import java.util.ArrayList;
 
 public class PCList {
-    private ArrayList<PC> pcList;
+    private static ArrayList<PC> pcList = new ArrayList<>();
 
-    public PCList() {
-        this.pcList = new ArrayList<>();
+    public static ArrayList<PC> getList() {
+        return pcList;
+    }
+
+    public static void printPcList() {
+        if (pcList.size() > 0) {
+            System.out.println("Here is the list of PC Builds:");
+            for (int i = 0; i < pcList.size(); i++) {
+                System.out.println((i + 1) + "." + pcList.get(i));
+            }
+        } else {
+            System.out.println("List currently empty!");
+        }
     }
 }
