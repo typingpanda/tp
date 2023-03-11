5e40057e015c697a94213d31a7305184b2a8cb75
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2023-03-12 01:48:31 +0800

    Resolve formatting error

--- src/test/java/seedu/bigpp/command/commoncommand/BackCommandTest.java
+++ src/test/java/seedu/bigpp/command/commoncommand/BackCommandTest.java
@@ -6,16 +6,16 @@ import seedu.bigpp.ui.UI;
 
 public class BackCommandTest {
 
-        @Test
-        public void executeCommand_backCommand_success() {
-                BackCommand backCommand = new BackCommand();
-                UI.setPCViewerMode();
-                String pcViewerBackCommandResult = backCommand.executeCommand();
-                assertEquals("Cannot go back from PCViewer", pcViewerBackCommandResult);
+    @Test
+    public void executeCommand_backCommand_success() {
+        BackCommand backCommand = new BackCommand();
+        UI.setPCViewerMode();
+        String pcViewerBackCommandResult = backCommand.executeCommand();
+        assertEquals("Cannot go back from PCViewer", pcViewerBackCommandResult);
 
-                //edits first pc in pclist
-                UI.setPCBuilderMode(0);
-                String pcBuilderBackCommandResult = backCommand.executeCommand();
-                assertEquals("returned to PCViewer", pcBuilderBackCommandResult);
-        }
+        //edits first pc in pclist
+        UI.setPCBuilderMode(0);
+        String pcBuilderBackCommandResult = backCommand.executeCommand();
+        assertEquals("returned to PCViewer", pcBuilderBackCommandResult);
+    }
 }
