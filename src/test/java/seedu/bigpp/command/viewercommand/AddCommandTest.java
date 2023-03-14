60fd41785bcc1fa30f006d9650c36423230c761e
DESKTOP-9Q6JJ25\Reynold-SL <reynoldlam041100@gmail.com>
2023-03-14 14:52:40 +0800

    Update JUnit test file with exception handling

--- src/test/java/seedu/bigpp/command/viewercommand/AddCommandTest.java
+++ src/test/java/seedu/bigpp/command/viewercommand/AddCommandTest.java
@@ -1,12 +1,13 @@
 package seedu.bigpp.command.viewercommand;
 
 import org.junit.jupiter.api.Test;
+import seedu.bigpp.exceptions.PPException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddCommandTest {
     @Test
-    public void executeCommand_addCommand_success() {
+    public void executeCommand_addCommand_success() throws PPException {
         String pcName = "Test PC";
         ViewerAddCommand viewerAddCommand = new ViewerAddCommand(pcName);
         String commandResult = viewerAddCommand.executeCommand();
