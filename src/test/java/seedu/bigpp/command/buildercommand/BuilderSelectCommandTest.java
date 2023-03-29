94d06dd9d7c88f29aa4308655179aad491c89964
typingpanda <chanxz@keane.gmail.com>
2023-03-29 15:54:05 +0800

    Add Junit test for component select

--- src/test/java/seedu/bigpp/command/buildercommand/BuilderSelectCommandTest.java
+++ src/test/java/seedu/bigpp/command/buildercommand/BuilderSelectCommandTest.java
@@ -10,9 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BuilderSelectCommandTest {
     private DataStorage dataStorage = new DataStorage();
 
-    private String expectedResultSuccess = "cpu added! : Intel core i3-10100";
-    private String expectedEmptyExceptionMessage = "Please select a component";
-
     @Test
     public void excecuteCommand_selectCommand_success() throws PPException {
         dataStorage.initStringToComponentListMap();
@@ -21,7 +18,7 @@ public class BuilderSelectCommandTest {
         UI.setPCBuilderMode(0);
 
         String selectCommandResult = new BuilderSelectCommand("cpu 1").executeCommand(dataStorage);
-        assertEquals(expectedResultSuccess, selectCommandResult);
+        assertEquals("cpu added! : Intel core i3-10100", selectCommandResult);
     }
 
     @Test
@@ -33,7 +30,64 @@ public class BuilderSelectCommandTest {
 
         Exception exception = assertThrows(PPException.class, () -> new BuilderSelectCommand("").executeCommand(
                 dataStorage));
-        assertEquals(expectedEmptyExceptionMessage,
+        assertEquals(
+                "Please select a component",
+                exception.getMessage());
+    }
+
+    @Test
+    public void executeCommand_selectInvalidComponent_exceptionThrown() {
+        dataStorage.initStringToComponentListMap();
+        dataStorage.loadAll();
+
+        UI.setPCBuilderMode(0);
+
+        Exception exception = assertThrows(PPException.class, () -> new BuilderSelectCommand("component")
+                .executeCommand(
+                        dataStorage));
+        assertEquals("Please select a valid component (cpu,gpu,ram,storage,psu,motherboard,cpu-cooler,chassis)",
+                exception.getMessage());
+    }
+
+    @Test
+    public void executeCommand_missingIndexOfComponent_exceptionThrown() {
+        dataStorage.initStringToComponentListMap();
+        dataStorage.loadAll();
+
+        UI.setPCBuilderMode(0);
+
+        Exception exception = assertThrows(PPException.class, () -> new BuilderSelectCommand("cpu")
+                .executeCommand(
+                        dataStorage));
+        assertEquals("Please input an index after selecting a component",
+                exception.getMessage());
+    }
+
+    @Test
+    public void executeCommand_invalidTypeForIndexOfComponent_exceptionThrown() {
+        dataStorage.initStringToComponentListMap();
+        dataStorage.loadAll();
+
+        UI.setPCBuilderMode(0);
+
+        Exception exception = assertThrows(PPException.class, () -> new BuilderSelectCommand("cpu a")
+                .executeCommand(
+                        dataStorage));
+        assertEquals("Please enter an integer",
+                exception.getMessage());
+    }
+
+    @Test 
+    public void executeCommand_outOFBoundsIndex_exceptionThrown() {
+        dataStorage.initStringToComponentListMap();
+        dataStorage.loadAll();
+
+        UI.setPCBuilderMode(0);
+
+        Exception exception = assertThrows(PPException.class, () -> new BuilderSelectCommand("cpu 100")
+                .executeCommand(
+                        dataStorage));
+        assertEquals("Please enter a valid index",
                 exception.getMessage());
     }
 }
